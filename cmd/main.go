01e4d9df68f84262bf11b5a913c8c40bc35e2dbf
Luke Sands <luke.sands@outlook.com>
2025-02-25 02:54:57 +1100

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,13 @@ import "github.com/d3code/xlog"
 
 func main() {
 
-	xlog.EnableConsole(xlog.LevelTrace, xlog.CallerLong, "", true)
+	xlog.EnableConsole(xlog.LevelTrace, xlog.CallerLong, "[TEST]", true)
 
 	xlog.Tracef("This is a trace message")
 	xlog.Debugf("This is a debug message")
 	xlog.Infof("This is an info message")
 	xlog.Warnf("This is a warn message")
+	xlog.Successf("This is a success message")
 	xlog.Errorf("This is an error message")
-	xlog.Fatalf("This is an error message")
+	xlog.Fatalf("This is a fatal message")
 }
